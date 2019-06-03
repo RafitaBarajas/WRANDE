928ce45f18978d73544041e83e162a94d27448d0
YaxVazquez <yx.vazquez@gmail.com>
2019-06-03 00:59:38 -0500

    conexion a la base, login y registro

---
+++ php/registro.php
@@ -0,0 +1,28 @@
+<?php
+    $conexion = mysqli_connect("localhost","root","","cero");
+    mysqli_query($conexion, "SET NAMES 'utf8'"); 
+ 
+    $nom = trim($_POST["nombre"]);
+    $apa = trim($_POST["APat"]);
+    $ama = trim($_POST["AMat"]);
+    $bd = trim($_POST["fecha"]);
+    $puesto = trim($_POST["puesto"]);
+    $email = trim($_POST["email"]);
+    $contra = trim($_POST["contra"]);
+
+    $sql = "call sp_registro('$nom','$apa', '$ama', '$bd', '$puesto', '$email', '$contra')";
+    $respuesta = mysqli_query($conexion,$sql);
+    $msj = "";
+
+    if($filas = mysqli_fetch_array($respuesta)){
+        $msj .= $filas["MSJ"];
+    }
+
+    if(strcasecmp($msj,'Has sido registrado')==0){
+    	echo 1;
+    } else if (strcasecmp($msj,'Este correo ya está registrado')==0){	
+    	echo 2;
+    } else {
+    	echo 0;
+    } 
+?>
