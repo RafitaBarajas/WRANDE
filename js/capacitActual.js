ead116a87fc8df381490f9c979ab8bf6d77fff8b
rafitabarajas <grafiti444@yahoo.com.mx>
2019-06-04 02:57:15 -0500

    Lista Capacitación y Actualización

---
+++ js/capacitActual.js
@@ -0,0 +1,132 @@
+$(document).ready(
+	function() {
+
+		var n = 0;
+		var m = 0;
+
+		$("#formCapacit").validetta({
+			showErrorMessages : true,
+			display : 'bubble',
+			bubblePosition: 'bottom',
+  			bubbleGapLeft: 40,
+  			bubbleGapTop: 5,
+  			realTime : true,
+  			onValid : function(){
+
+  			}
+		});
+
+		$("#formAct").validetta({
+			showErrorMessages : true,
+			display : 'bubble',
+			bubblePosition: 'bottom',
+  			bubbleGapLeft: 40,
+  			bubbleGapTop: 5,
+  			realTime : true,
+  			onValid : function(){
+
+  			}
+		});
+
+		newCap();
+		newAct();
+
+		$("#rowCapacit").hide();
+		$("#rowAct").hide();
+
+		$("#moreCap").on("click", function(){
+			newCap();
+		});
+
+		$("#moreAct").on("click", function(){
+			newAct();
+		});
+
+		function newCap(){
+			var clon = $("#rowCapacit").clone();
+
+			n=n+1;
+
+			clon.removeAttr("id").attr("id","rowCapacit"+n);
+
+			clon.find("[id='tipoCap0']").removeAttr("id").attr("id","tipoCap"+n);
+			clon.find("[name='tipoCap0']").removeAttr("name").attr("name","tipoCap"+n);
+			clon.find("[for='tipoCap0']").removeAttr("for").attr("for","tipoCap"+n);
+
+			clon.find("[id='instCap0']").removeAttr("id").attr("id","instCap"+n);
+			clon.find("[name='instCap0']").removeAttr("name").attr("name","instCap"+n);
+			clon.find("[for='instCap0']").removeAttr("for").attr("for","instCap"+n);
+
+			clon.find("[id='paisCap0']").removeAttr("id").attr("id","paisCap"+n);
+
+			clon.find("[id='yrCap0']").removeAttr("id").attr("id","yrCap"+n);
+
+			clon.find("[id='hrsCap0']").removeAttr("id").attr("id","hrsCap"+n);
+			clon.find("[name='hrsCap0']").removeAttr("name").attr("name","hrsCap"+n);
+			clon.find("[for='hrsCap0']").removeAttr("for").attr("for","hrsCap"+n);
+
+			if(n!=1){
+				clon.find("i.fa-plus").removeClass("fa-plus").addClass("fa-minus").attr("data-value", n);
+			} else {
+				clon.find("i.fa-plus").attr("id", "moreCap");
+			}
+
+			clon.find("select").addClass("newSelect");
+
+			clon.appendTo("#baseCapacit").show();
+
+			$(".newSelect").formSelect();
+
+			$(".newSelect").removeClass("newSelect");
+		}
+
+		function newAct(){
+			var clon = $("#rowAct").clone();
+
+			m=m+1;
+
+			clon.removeAttr("id").attr("id","rowAct"+m);
+
+			clon.find("[id='tipoAct0']").removeAttr("id").attr("id","tipoAct"+m);
+			clon.find("[name='tipoAct0']").removeAttr("name").attr("name","tipoAct"+m);
+			clon.find("[for='tipoAct0']").removeAttr("for").attr("for","tipoAct"+m);
+
+			clon.find("[id='instAct0']").removeAttr("id").attr("id","instAct"+m);
+			clon.find("[name='instAct0']").removeAttr("name").attr("name","instAct"+m);
+			clon.find("[for='instAct0']").removeAttr("for").attr("for","instAct"+m);
+
+			clon.find("[id='paisAct0']").removeAttr("id").attr("id","paisAct"+m);
+
+			clon.find("[id='yrAct0']").removeAttr("id").attr("id","yrAct"+m);
+
+			clon.find("[id='hrsAct0']").removeAttr("id").attr("id","hrsAct"+m);
+			clon.find("[name='hrsAct0']").removeAttr("name").attr("name","hrsAct"+m);
+			clon.find("[for='hrsAct0']").removeAttr("for").attr("for","hrsAct"+m);
+
+			if(m!=1){
+				clon.find("i.fa-plus").removeClass("fa-plus").addClass("fa-minus").attr("data-value", m);
+			} else {
+				clon.find("i.fa-plus").attr("id", "moreAct");
+			}
+
+			clon.find("select").addClass("newSelect");
+
+			clon.appendTo("#baseAct").show();
+
+			$(".newSelect").formSelect();
+
+			$(".newSelect").removeClass("newSelect");
+		}
+
+		$(document).on("click", ".delCap", function(){
+			var v = $(this).find("i.fa-minus").data("value");
+			$("#rowCapacit"+v).remove();
+		});
+
+		$(document).on("click", ".delAct", function(){
+			var v = $(this).find("i.fa-minus").data("value");
+			$("#rowAct"+v).remove();
+		});
+
+	}
+);
---
+++ js/formacion.js
@@ -0,0 +1,71 @@
+$(document).ready(
+	function() {
+
+		var n = 0;
+
+		$("#formFA").validetta({
+			showErrorMessages : true,
+			display : 'bubble',
+			bubblePosition: 'bottom',
+  			bubbleGapLeft: 40,
+  			bubbleGapTop: 5,
+  			realTime : true,
+  			onValid : function(){
+
+  			}
+		});
+
+		newFA();
+		$("#rowFA").hide();
+
+		$("#moreFA").on("click", function(){
+			newFA();
+		});
+
+		function newFA(){
+			var clon = $("#rowFA").clone();
+
+			n=n+1;
+
+			clon.removeAttr("id").attr("id","rowFA"+n);
+
+			clon.find("[id='nvl0']").removeAttr("id").attr("id","nvl"+n);
+
+			clon.find("[id='nombre0']").removeAttr("id").attr("id","nombre"+n);
+			clon.find("[name='nombre0']").removeAttr("name").attr("name","nombre"+n);
+			clon.find("[for='nombre0']").removeAttr("for").attr("for","nombre"+n);
+
+			clon.find("[id='inst0']").removeAttr("id").attr("id","inst"+n);
+			clon.find("[name='inst0']").removeAttr("name").attr("name","inst"+n);
+			clon.find("[for='inst0']").removeAttr("for").attr("for","inst"+n);
+
+			clon.find("[id='pais0']").removeAttr("id").attr("id","pais"+n);
+
+			clon.find("[id='yr0']").removeAttr("id").attr("id","yr"+n);
+
+			clon.find("[id='ced0']").removeAttr("id").attr("id","ced"+n);
+			clon.find("[name='ced0']").removeAttr("name").attr("name","ced"+n);
+			clon.find("[for='ced0']").removeAttr("for").attr("for","ced"+n);
+
+			if(n!=1){
+				clon.find("i.fa-plus").removeClass("fa-plus").addClass("fa-minus").attr("data-value", n);
+			} else {
+				clon.find("i.fa-plus").attr("id", "moreFA");
+			}
+
+			clon.find("select").addClass("newSelect");
+
+			clon.appendTo("#baseFA").show();
+
+			$(".newSelect").formSelect();
+
+			$(".newSelect").removeClass("newSelect");
+		}
+
+		$(document).on("click", ".del", function(){
+			var v = $(this).find("i.fa-minus").data("value");
+			$("#rowFA"+v).remove();
+		});
+
+	}
+);
