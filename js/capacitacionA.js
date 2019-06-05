4b9c5c78d18f886d6807a303152b2298ae8af798
YaxVazquez <yx.vazquez@gmail.com>
2019-06-05 00:32:44 -0500

    inserts actualizacion

--- js/capacitacionA.js
+++ js/capacitacionA.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
 			            data: {string:jsonString},
 			            cache:false,
 			            success:function(resp){
-			            	alert(resp);
 			                var Jresp=$.parseJSON(resp);
 			                if(Jresp["estado"]=="1"){
 			                    $.alert({
