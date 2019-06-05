425c3723f4384f60584e52305674289003be36d1
DiegoACV <diego.colon3318@gmail.com>
2019-06-04 20:01:13 -0500

    perfil

---
+++ php/perfil.php
@@ -0,0 +1,25 @@
+<?php
+    $conexion = mysqli_connect("localhost","root","","cero");
+    mysqli_query($conexion, "SET NAMES 'utf8'"); 
+
+    $mail = "t@t.t";//CAMBIAR POR SESSION
+
+    $sql = "call sp_datosPerfil('$mail')";
+    $respuesta = mysqli_query($conexion,$sql);
+
+    $faux = "";
+
+    $array = array();
+
+    if($filas = mysqli_fetch_array($respuesta)){
+
+	    $array["nombre"] = $filas["nombre"];
+	    $array["apaterno"] = $filas["apaterno"];
+	    $array["amaterno"] = $filas["amaterno"];
+	    $array["edad"]  = $filas["edad"];
+	    $array["puesto"]  = $filas["puesto"];
+	    $array["email"]  = $filas["email"];
+    }
+
+    echo json_encode($array);
+?>
